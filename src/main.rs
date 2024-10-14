7fe8826cda7855e6b19253d7bd8b07db17ff1a00
r13ef <73205763+r13ef@users.noreply.github.com>
2024-10-14 11:33:17 +0900

    Minor change

--- src/main.rs
+++ src/main.rs
@@ -211,13 +211,14 @@ fn main() {
 
     let mut inter_list = InteractionsModEquiv::new(n);
     inter_list.create_list();
-    println!("{}", inter_list.my_inter_list.len());
-    // let file_name = String::from("output/size_4.json");
-    // let result = inter_list.output_json(file_name);
-    // match result {
-    // Ok(..) => {}
-    // Err(err) => {
-    // println!("{err}");
-    // }
-    // }
+    let mut file_name = String::from("output/size_");
+    file_name.push_str(&(n as i32).to_string());
+    file_name.push_str(".json");
+    let result = inter_list.output_json(file_name);
+    match result {
+        Ok(..) => {}
+        Err(err) => {
+            println!("{err}");
+        }
+    }
 }
